639a57f25b1d854f0d39a59d548d139a50b9e4ae
myname21-meet <myname21@meet.mit.edu>
2019-07-31 06:21:26 -0400

    hi

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from databases import *
+from flask import Flask, render_template, url_for, request
 app = Flask(__name__)
 
+# 127.0.0.1:5000/ -> opens 'hello.html'
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def hello_html():
+    return render_template("hello.html")
 
-if __name__ == '__main__':
+
+
+
+if __name__ == '__main__': # This is always at the end
     app.run(debug=True)
-
---
+++ model.py
@@ -0,0 +1,6 @@
+# from sqlalchemy import Column, Integer, String, Boolean
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import relationship, sessionmaker
+# from sqlalchemy import create_engine
+
+# Base = declarative_base()
